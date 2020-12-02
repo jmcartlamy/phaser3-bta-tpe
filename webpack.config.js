9879d17ebe491d17aab43d56d6d13a0ca6e3b767
jmcartlamy <contact@cartlamy.com>
2020-12-02 17:48:11 +0100

    chore: add hot module replacement plugin

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,7 @@ module.exports = {
       CANVAS_RENDERER: JSON.stringify(true),
       WEBGL_RENDERER: JSON.stringify(true)
     }),
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       template: './index.html'
     })
